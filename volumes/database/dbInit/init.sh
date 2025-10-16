1601d99ae26daf0ba31e9f33a27409807e2da507
mozzy11 <mozzymutesa@gmail.com>
2025-10-16 16:51:20 +0300

    fix ci build error

--- volumes/database/dbInit/init.sh
+++ volumes/database/dbInit/init.sh
@@ -5,7 +5,7 @@ set -e
 : "${DB_PASSWORD:=clinlims}"         # default database name
 : "${DB_SUPERUSER_PASSWORD:=superuser}"         # default admin password
 # Substitute environment variables in SQL template
-envsubst < /docker-entrypoint-initdb.d/1-pgsqlPermissions.sql > /docker-entrypoint-initdb.d/1-pgsqlPermissions.sql
+envsubst < /docker-entrypoint-initdb.d/1-pgsqlPermissions.sql.template > /docker-entrypoint-initdb.d/1-pgsqlPermissions.sql
 
 # Execute the generated SQL
 psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/1-pgsqlPermissions.sql
